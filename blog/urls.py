60a9a21a2d012538ac952558c45af9f2ef0ee68a
unknown <sonychajt@gmail.com>
2023-07-21 21:39:50 -0300

    Proyecto inicial django

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import index, contacto
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", index, name = "index"),
+    path("contacto/", contacto, name = "contacto"),
+]
